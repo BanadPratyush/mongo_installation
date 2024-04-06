dddf0facb0309caed09252acb4d6fba419c91e3e
BanadPratyush <31466703+BanadPratyush@users.noreply.github.com>
2024-04-06 23:45:27 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -4,6 +4,6 @@ sudo rm /etc/apt/sources.list.d/rethinkdb.list.save.*
 sudo rm /etc/apt/sources.list.d/signal-xenial.list
 rm /etc/apt/sources.list.d/mongodb-org-7.0.list*
 touch /etc/apt/sources.list.d/mongodb-org-7.0.list
-echo \"deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse\" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+echo ""deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse"" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
