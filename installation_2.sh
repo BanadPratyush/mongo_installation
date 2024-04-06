88e28524de816bad948e9cb4c60e3cce1a833caa
root <root@tanishq-vm.us-central1-a.c.contentm-412106.internal>
2024-04-06 18:24:54 +0000

    second installation script

---
+++ installation_2.sh
@@ -0,0 +1,8 @@
+sudo apt-get install gnupg curl
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
+touch  /etc/apt/sources.list.d/mongodb-org-7.0.list
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+systemctl enable mongod
+systemctl start mongod
