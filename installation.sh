4b0a33e63f0dd62906eadb769f5a58b602214a7b
BanadPratyush <31466703+BanadPratyush@users.noreply.github.com>
2024-04-06 23:43:45 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -2,7 +2,7 @@ sudo apt-get install gnupg curl
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
 sudo rm /etc/apt/sources.list.d/rethinkdb.list.save.*
 sudo rm /etc/apt/sources.list.d/signal-xenial.list
-rm /etc/apt/sources.list.d/mongodb-org-7.0.list
+rm /etc/apt/sources.list.d/mongodb-org-7.0.list*
 touch /etc/apt/sources.list.d/mongodb-org-7.0.list
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 sudo apt-get update
